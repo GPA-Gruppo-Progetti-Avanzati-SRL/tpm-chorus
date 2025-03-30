a34c9f1002f53ef2fe55009957fc26fc8a76a4ab
M.A.I <mario.imperato@gpagroup.it>
2025-03-30 19:40:26 +0200

    spostamento aggiornamento configurazioni pipeline

--- pipeline/executable/sinks/kafkasink/types.go
+++ pipeline/executable/sinks/kafkasink/types.go
@@ -16,9 +16,10 @@ type StatsInfo struct {
 	Status200Metric promutil.CollectorWithLabels
 	Status202Metric promutil.CollectorWithLabels
 	Status500Metric promutil.CollectorWithLabels
+	metricErrors    bool
 }
 
-func (stat StatsInfo) Clear() StatsInfo {
+func (stat *StatsInfo) Clear() *StatsInfo {
 	stat.NumQueuedMessages = 0
 	stat.NumProducedMessages = 0
 	stat.NumAcceptedMessages = 0
@@ -26,19 +27,66 @@ func (stat StatsInfo) Clear() StatsInfo {
 	return stat
 }
 
-func (stat StatsInfo) IsZero() bool {
+const (
+	MetricLabelName       = "name"
+	MetricLabelStatusCode = "status-code"
+	MetricLabelTopicName  = "topic-name"
+)
+
+func NewStatsInfo(brokerName, topicName, metricGroupId, metricId string) *StatsInfo {
+	stat := &StatsInfo{}
+	mg, err := promutil.GetGroup(metricGroupId)
+	if err != nil {
+		stat.metricErrors = true
+		return stat
+	} else {
+		stat.Status200Metric, err = mg.CollectorByIdWithLabels(metricId, map[string]string{
+			MetricLabelName:       brokerName,
+			MetricLabelStatusCode: "200",
+			MetricLabelTopicName:  "topic-name",
+		})
+		if err != nil {
+			stat.metricErrors = true
+			return stat
+		}
+
+		stat.Status202Metric, err = mg.CollectorByIdWithLabels(metricId, map[string]string{
+			MetricLabelName:       brokerName,
+			MetricLabelStatusCode: "202",
+			MetricLabelTopicName:  "topic-name",
+		})
+		if err != nil {
+			stat.metricErrors = true
+			return stat
+		}
+
+		stat.Status500Metric, err = mg.CollectorByIdWithLabels(metricId, map[string]string{
+			MetricLabelName:       brokerName,
+			MetricLabelStatusCode: "500",
+			MetricLabelTopicName:  "topic-name",
+		})
+		if err != nil {
+			stat.metricErrors = true
+			return stat
+		}
+	}
+
+	return stat
+}
+
+func (stat *StatsInfo) IsZero() bool {
 	return stat.NumQueuedMessages == 0 && stat.NumProducedMessages == 0 && stat.NumAcceptedMessages == 0 && stat.NumFailedMessages == 0
 }
 
-func (stat StatsInfo) IsFail() bool {
+func (stat *StatsInfo) IsFail() bool {
 	return stat.NumFailedMessages > 0
 }
 
-func (stat StatsInfo) IsComplete() bool {
+func (stat *StatsInfo) IsComplete() bool {
 	return stat.NumQueuedMessages == (stat.NumProducedMessages + stat.NumFailedMessages)
 }
 
-func (stat StatsInfo) Log(semLogContext string, expectedCompleted bool) {
+func (stat *StatsInfo) Log(semLogContext string, expectedCompleted bool) {
 	var err error
 	var evt *zerolog.Event
 	if stat.IsFail() {
@@ -57,7 +105,7 @@ func (stat StatsInfo) Log(semLogContext string, expectedCompleted bool) {
 		Uint64("num-queued", stat.NumQueuedMessages).Msg(semLogContext)
 }
 
-func (stat StatsInfo) IsErr(expectedCompleted bool) error {
+func (stat *StatsInfo) IsErr(expectedCompleted bool) error {
 	var err error
 	if stat.IsFail() {
 		err = errors.New("kafka stage with some failures")
@@ -66,4 +114,12 @@ func (stat StatsInfo) IsErr(expectedCompleted bool) error {
 	}
 
 	return err
+}
+
+func (stat *StatsInfo) ProduceMetrics() {
+	if !stat.metricErrors {
+		stat.Status202Metric.SetMetric(float64(stat.NumAcceptedMessages))
+		stat.Status200Metric.SetMetric(float64(stat.NumProducedMessages))
+		stat.Status500Metric.SetMetric(float64(stat.NumFailedMessages))
+	}
 }
